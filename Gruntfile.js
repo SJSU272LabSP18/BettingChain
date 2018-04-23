5ada633a73ea4bb9f81eb680d8dbaa49c9626254
Aliaksei Matsarski <alex.matsarski@getzephyr.com>
2018-04-22 17:37:41 -0700

    Fix grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,15 +33,15 @@ module.exports = function(grunt) {
             push_docker_image: {
                 command: [
                 //    'echo <%= secret.password %> | sudo -S whoami',
-                    'echo "docker.devops"|docker login',
+                //    'echo "gshivani08"|docker login',
                     'docker push gshivani08/blockchain-lottery:<%= gitinfo.my.custom.command %>',
-                    'echo "docker.devops"|docker login',
+                //    'echo "docker.devops"|docker login',
                     'docker push gshivani08/blockchain-lottery:<%= gitinfo.my.custom.command %>'
                 ].join('&&')
             }
         },
         // Pass file name as argument to call this task e.g grunt --config qa shell
-        secret: grunt.file.readJSON(grunt.option("config")+'.json'),
+      //  secret: grunt.file.readJSON(grunt.option("config")+'.json'),
         sshexec: {
             //Placeholder for future
         }
