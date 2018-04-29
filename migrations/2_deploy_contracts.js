b8c8c3bd90da3a691af7db4e18870759d4bd58c4
Shivani <cosmogal_shivani@yahoo.com>
2018-04-28 17:46:34 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var BettingEther= artifacts.require("./BettingEther.sol");
-module.exports = function(deployer) {
-  deployer.deploy(BettingEther);
-}
+var BettingEtherColor= artifacts.require("./BettingEtherColor.sol");
+var BettingEtherDigit= artifacts.require("./BettingEtherDigit.sol");
 
+module.exports = function(deployer) {
+    deployer.deploy(BettingEtherColor,BettingEtherDigit);
+    //deployer.deploy(BettingEtherDigit);
+}
