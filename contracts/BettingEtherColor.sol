6456904f310e98f25f5e6316be67728c80973ae6
Aliaksei Matsarski <alex.matsarski@getzephyr.com>
2018-04-28 22:23:30 -0700

    Remove _betting_amount from insert_bet_color(), since we have the ether value in msg.value

--- contracts/BettingEtherColor.sol
+++ contracts/BettingEtherColor.sol
@@ -51,32 +51,20 @@ contract BettingEtherColor {
 
 
   //insert bet for color game
-  function insert_bet_color(string _betting_choice_color, uint _betting_amount) payable public {
-
-    require(_betting_amount == msg.value);
-    require(isGameRunning == true);
-    require(keccak256('RED') == keccak256(_betting_choice_color) || keccak256('BLACK') == keccak256(_betting_choice_color));
-    ticketCounter++;
-    totalBettingAmountColorGame += _betting_amount;
-    uint colorCode;
-    if(keccak256('RED') == keccak256(_betting_choice_color))
-    {
-      colorCode=0;
-    }
-    else
-    {
-      colorCode=1;
-    }
-
-    colorTotalAmount[colorCode] += _betting_amount;
-    bets[ticketCounter] = Bet(
-      ticketCounter,
-      _betting_amount,
-      _betting_choice_color,
-      msg.sender
-    );
-
-    LogInsertColorBet(ticketCounter,_betting_amount,_betting_choice_color,msg.sender);
+  function insert_bet_color(string _betting_choice_color) payable public {
+      require(isGameRunning == true);
+      require(keccak256('RED') == keccak256(_betting_choice_color) || keccak256('BLACK') == keccak256(_betting_choice_color));
+      ticketCounter++;
+      totalBettingAmountColorGame += msg.value;
+      uint colorCode;
+      if(keccak256('RED') == keccak256(_betting_choice_color)) {
+          colorCode=0;
+      } else {
+          colorCode=1;
+      }
+      colorTotalAmount[colorCode] += msg.value;
+      bets[ticketCounter] = Bet(ticketCounter, msg.value, _betting_choice_color, msg.sender);
+      LogInsertColorBet(ticketCounter, msg.value, _betting_choice_color, msg.sender);
   }
 
 
